602a12914be711e9e6daf7fa572d0bc0d2498c97
Jaime Bacotich <jaimebacotich@gmail.com>
2026-01-28 23:23:22 -0400

    feat: inyectar fallo intencional para demo de CI

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('dashboard'));
+
+// 💣 SIMULACIÓN DE FALLO DE SINTAXIS
+Route::get('/fallo', function () {
+    return "Esto falla" // <--- FALTA PUNTO Y COMA
+})
